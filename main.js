a2bda660baa5493cabeab80b0aa3c0ba059d8487
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2023-12-09 23:54:37 +0600

    added the city name, city time and city temp

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const searchBtn = document.querySelector('#search-btn');
 const inputBtn = document.querySelector('#enter-city');
 
+const city = document.querySelector('#city-name');
+const cityTime = document.querySelector('#city-time');
+const cityTem = document.querySelector('#city-temp');
+
 async function apiData(cityName) {
   const promise =
     await fetch(`http://api.weatherapi.com/v1/current.json?key=6c2dfad1edbf4bc78de145717230912&q=${cityName}&aqi=yes
